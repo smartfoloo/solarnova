3d682269d46c989cc551412e8e9f315d5367aa1b
hotmayonaise <82243014+GinoGeremia@users.noreply.github.com>
2023-12-05 19:01:51 -0600

    GET JOLLY WITH IT

--- app.js
+++ app.js
@@ -34,13 +34,13 @@ document.addEventListener('DOMContentLoaded', () => {
 		'lofi-jazz': ['from-the-start','cupid','circus', 'that-kyoto-vibe', 'brazilian-beach-rumba', 'kyoto-nights', 'cactus-cafe', 'coffee-moments', 'jazz-in-my-coffee', 'sushi'],
 		'seasonal': ['mariahcarey', 'snowman'],
 		'mix': ['paint-the-town-red', 'somebody-that-i-used-to-know', 'somebodys-watching-me', 'ballin', 'bad-habit', 'luxury', 'everybody-wants-to-rule-the-world', 'the-box', 'the-perfect-girl'],
-		'rap': ['lovin-on-me','all-girls-are-the-same', 'the-box', 'ballin','fkumean','no-role-modelz','solo','what-are-you-so-afraid-of','chain-gang','wet-dreamz','passionfruit','teenage-fever','i-hate-police','great-gatsby','butterfly-effect','space-cadet','zeze'],
+		'rap': ['lovin-on-me','all-girls-are-the-same', 'the-box', 'ballin','fkumean','no-role-modelz','solo','bad','what-are-you-so-afraid-of','chain-gang','wet-dreamz','passionfruit','teenage-fever','i-hate-police','great-gatsby','butterfly-effect','space-cadet','zeze','not-fair'],
 		'pop1': ['paint-the-town-red', 'somebody-that-i-used-to-know', 'somebodys-watching-me', 'ballin', 'bad-habit', 'sleepwalker','luxury', 'everybody-wants-to-rule-the-world', 'the-box', 'the-perfect-girl'],
 		'classical': ['violin-concerto-in-e-minor', 'adagio-albinoni', 'trio-no-2-in-e-minor', 'moonlight-sonata'],
     'national-anthems': ['soviet-anthem', 'heil-dir-im-siegerkranz', 'horst-wessel-lied', 'deutschlandlied', 'la-marseillaise'],
 		'tth': ['greedy','it-girl','ecstasy','moonlight','only-in-my-mind','strangers','smooth-operator-(tiktok-remix)'],
 		'drakeywakey' :['passionfruit','teenage-fever','idgaf-(frick)'],
-		'xxxtentacion' :['sad!','all-girls-are-the-same','what-are-you-so-afraid-of']
+		'xxxtentacion' :['sad!','all-girls-are-the-same','what-are-you-so-afraid-of','bad']
 	}
 
 
@@ -163,6 +163,8 @@ document.addEventListener('DOMContentLoaded', () => {
 				'butterfly-effect': 'travis scott',
 				'great-gatsby': 'rod wave',
 				'space-cadet': 'metro boomin',
+				'not-fair': 'the kid laroi',
+				'bad': 'xxxtentacion',
 				// pop
 				'paint-the-town-red': '✨💅doja cat!1!💅✨',
 				'somebody-that-i-used-to-know': 'gotye (official aussie)',
