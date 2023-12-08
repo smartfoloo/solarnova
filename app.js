f720937a2eedb39d86e96c098fba31f0e995c8bf
hotmayonaise <82243014+GinoGeremia@users.noreply.github.com>
2023-12-07 18:10:08 -0600

    sss

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		'lofi-jazz': ['from-the-start','cupid','circus', 'that-kyoto-vibe', 'brazilian-beach-rumba', 'kyoto-nights', 'cactus-cafe', 'coffee-moments', 'jazz-in-my-coffee', 'sushi'],
 		'seasonal': ['mariahcarey', 'snowman'],
 		'mix': ['paint-the-town-red', 'somebody-that-i-used-to-know', 'somebodys-watching-me', 'ballin', 'bad-habit', 'luxury', 'everybody-wants-to-rule-the-world', 'the-box', 'the-perfect-girl'],
-		'rap': ['lovin-on-me','all-girls-are-the-same', 'the-box', 'ballin','fkumean','no-role-modelz','solo','bad','what-are-you-so-afraid-of','chain-gang','wet-dreamz','passionfruit','teenage-fever','i-hate-police','great-gatsby','butterfly-effect','space-cadet','zeze','not-fair','223s','look-back-at-it'],
+		'rap': ['lovin-on-me','all-girls-are-the-same', 'the-box', 'ballin','fkumean','no-role-modelz','solo','bad','what-are-you-so-afraid-of','chain-gang','wet-dreamz','passionfruit','teenage-fever','i-hate-police','great-gatsby','butterfly-effect','space-cadet','zeze','not-fair','223s','look-back-at-it','humble','like-this','overtime','rockstar','swervin'],
 		'pop1': ['paint-the-town-red', 'somebody-that-i-used-to-know', 'somebodys-watching-me', 'ballin', 'bad-habit', 'sleepwalker','luxury', 'everybody-wants-to-rule-the-world', 'the-box', 'the-perfect-girl'],
 		'classical': ['violin-concerto-in-e-minor', 'adagio-albinoni', 'trio-no-2-in-e-minor', 'moonlight-sonata'],
     'national-anthems': ['soviet-anthem', 'heil-dir-im-siegerkranz', 'horst-wessel-lied', 'deutschlandlied', 'la-marseillaise'],
@@ -170,6 +170,8 @@ document.addEventListener('DOMContentLoaded', () => {
 				'swervin': 'a boogie wit da hoodie',
 				'rockstar': 'da baby',
 				'humble': 'kendrick lamar',
+				'overtime': 'ken carson',
+				'like-this': 'ken carson',
 				// pop
 				'paint-the-town-red': '✨💅doja cat!1!💅✨',
 				'somebody-that-i-used-to-know': 'gotye (official aussie)',
