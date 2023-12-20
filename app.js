45d7a0a3d715190018d85a8bf1d7b753c2336fff
platformerdotio <smartfoloo@gmail.com>
2023-12-19 19:04:10 -0600

    g

--- app.js
+++ app.js
@@ -682,3 +682,102 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+var tab = localStorage.getItem("tab");
+
+if (tab) {
+  try {
+    var tabData = JSON.parse(tab);
+  } catch {
+    var tabData = {};
+  }
+} else {
+  var tabData = {};
+}
+
+if (tabData.title) {
+  document.getElementById("title").value = tabData.title;
+}
+if (tabData.icon) {
+  document.getElementById("icon").value = tabData.icon;
+}
+
+var settingsDefaultTab = {
+  title: "Music Player",
+  icon: "/favicon.png",
+};
+
+function setTitle(title = "") {
+  if (title) {
+    document.title = title;
+  } else {
+    document.title = settingsDefaultTab.title;
+  }
+
+  var tab = localStorage.getItem("tab");
+
+  if (tab) {
+    try {
+      var tabData = JSON.parse(tab);
+    } catch {
+      var tabData = {};
+    }
+  } else {
+    var tabData = {};
+  }
+
+  if (title) {
+    tabData.title = title;
+  } else {
+    delete tabData.title;
+  }
+
+  localStorage.setItem("tab", JSON.stringify(tabData));
+}
+
+function setFavicon(icon) {
+  if (icon) {
+    document.querySelector("link[rel='icon']").href = icon;
+  } else {
+    document.querySelector("link[rel='icon']").href = settingsDefaultTab.icon;
+  }
+
+  var tab = localStorage.getItem("tab");
+
+  if (tab) {
+    try {
+      var tabData = JSON.parse(tab);
+    } catch {
+      var tabData = {};
+    }
+  } else {
+    var tabData = {};
+  }
+
+  if (icon) {
+    tabData.icon = icon;
+  } else {
+    delete tabData.icon;
+  }
+
+  localStorage.setItem("tab", JSON.stringify(tabData));
+}
+
+var tab = localStorage.getItem('tab');
+if (tab) {
+  try {
+    var tabData = JSON.parse(tab);
+  } catch {
+    var tabData = {};
+  }
+} else {
+  var tabData = {};
+}
+
+if (tabData.title) {
+  document.title = tabData.title;
+}
+
+if (tabData.icon) {
+  document.querySelector('link[rel="icon"]').href = tabData.icon;
+}
